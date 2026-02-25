018465c40d6825d421985d2616bea2c4c6f097f2
David Cui <david_cui@brown.edu>
2026-02-25 04:37:44 -0500

    seo:

--- src/components/hero/HeroTitle.tsx
+++ src/components/hero/HeroTitle.tsx
@@ -1,7 +1,7 @@
 export function HeroTitle() {
   return (
     <div className="flex flex-col items-center pb-8 pt-4 md:pb-12 md:pt-6">
-      <p
+      <h1
         className="text-center font-fenway text-yellow-400
                    text-[12vw] leading-tight
                    md:whitespace-nowrap md:text-[12vw] lg:text-[9vw] xl:text-[7vw]"
@@ -11,7 +11,7 @@ export function HeroTitle() {
         }}
       >
         Laissez les bon temps rouler!
-      </p>
+      </h1>
       <a
         href="tel:+14699865555"
         className="mt-8 inline-flex items-center justify-center rounded-sm border border-white/70
